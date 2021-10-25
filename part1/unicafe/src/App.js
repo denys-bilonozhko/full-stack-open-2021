9689dc08d19a24c86db21c36ac89e03d4fa55dcc
Denys Bilonozhko <deformix@gmail.com>
2021-10-25 21:29:34 +0300

    1.11*: unicafe step6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,22 +6,34 @@ const Button = ({ text, handler }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <p>
-      {text} {value}
-    </p>
+    <tr>
+      <td> {text} </td>
+      <td> {value} </td>
+    </tr>
   );
 };
 
-const Statistics = (props) => {
+const Statistics = ({ feedback }) => {
+  const total = feedback.good + feedback.neutral + feedback.bad;
   return (
     <>
       <h2>Statistics</h2>
-      <StatisticLine text="good" value={'6'} />
-      <StatisticLine text="neutral" value={'2'} />
-      <StatisticLine text="bad" value={'1'} />
-      <StatisticLine text="all" value={'9'} />
-      <StatisticLine text="average" value={'0.555555'} />
-      <StatisticLine text="positive" value={'66.66666 %'} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={feedback.good} />
+          <StatisticLine text="neutral" value={feedback.neutral} />
+          <StatisticLine text="bad" value={feedback.bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine
+            text="average"
+            value={(feedback.good - feedback.bad) / total}
+          />
+          <StatisticLine
+            text="positive"
+            value={(feedback.good / total) * 100}
+          />
+        </tbody>
+      </table>
     </>
   );
 };
@@ -34,10 +46,14 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button text="good" />
-      <Button text="neutral" />
-      <Button text="bad" />
-      {good || neutral || bad ? <Statistics /> : <p>No feedback given</p>}
+      <Button text="good" handler={() => setGood(good + 1)} />
+      <Button text="neutral" handler={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handler={() => setBad(bad + 1)} />
+      {good || neutral || bad ? (
+        <Statistics feedback={{ good, neutral, bad }} />
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
   );
 };
