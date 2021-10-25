704fece2fea086b999c731266c6cda1230fd5188
Denys Bilonozhko <deformix@gmail.com>
2021-10-25 21:01:55 +0300

    1.10: unicafe step5

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
 
+const Button = ({ text, handler }) => {
+  return <button onClick={handler}>{text}</button>;
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
 const Statistics = (props) => {
   return (
     <>
       <h2>Statistics</h2>
-      <p>good 6</p>
-      <p>neutral 2</p>
-      <p>bad 1</p>
-      <p>all 9</p>
-      <p>average 0.555555</p>
-      <p>positive 66.66666 %</p>
+      <StatisticLine text="good" value={'6'} />
+      <StatisticLine text="neutral" value={'2'} />
+      <StatisticLine text="bad" value={'1'} />
+      <StatisticLine text="all" value={'9'} />
+      <StatisticLine text="average" value={'0.555555'} />
+      <StatisticLine text="positive" value={'66.66666 %'} />
     </>
   );
 };
@@ -22,9 +34,9 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <Button text="good" />
+      <Button text="neutral" />
+      <Button text="bad" />
       {good || neutral || bad ? <Statistics /> : <p>No feedback given</p>}
     </div>
   );
